509a98a93dda328f26b05c14c75b7f9e1d2e922e
unknown <Szilgyi Szabolcs@NET1.LOCAL>
2025-11-27 12:49:04 +0100

    javascript questions

--- quiz.js
+++ quiz.js
@@ -5,17 +5,11 @@ const kvizKerdes = [
         answer: ["I. Ferenc","V. Ferdinánd","Ferenc József","I. Lipót"],
         correct: 1
     },
-        {
-        subject: "Történelem",
-        question: "Mi volt az áprilisi törvények egyik fő célja?",
-        answer: ["A Habsburg-ház megerősítése","A polgári átalakulás megteremtése","A három részre szakadt ország újraegyesítése","Az ország teljes függetlenségének kikiáltása"],
-        correct: 1
-    },
-        {
-        subject: "Történelem",
-        question: "Melyik reform NEM szerepelt az áprilisi törvények között?",
-        answer: ["A jobbágyság eltörlése","A közteherviselés bevezetése","Általános választójog mindenkinek","A felelős magyar kormány létrehozása"],
-        correct: 2
+    {
+        subject: "Magyar nyelv",
+        question: "Melyik történelmi korszak NEM jelenik meg a mű színeiben?",
+        answer: ["Ókori Athén","Középkori Prága","Római Birodalom","Amerikai vadnyugat"],
+        correct: 3
     },
     {
         subject: "Matematika",
@@ -24,16 +18,16 @@ const kvizKerdes = [
         correct: 3
     },
     {
+        subject: "Történelem",
+        question: "Melyik reform NEM szerepelt az áprilisi törvények között?",
+        answer: ["A jobbágyság eltörlése","A közteherviselés bevezetése","Általános választójog mindenkinek","A felelős magyar kormány létrehozása"],
+        correct: 2
+    },
+    {
         subject: "Matematika",
         question: "Melyik egyenlőség helyes?",
         answer: ["cos 0° = 0","sin 90° = 1","tan 45° = 0","cos 60° = √3/2"],
         correct: 1
-    },
-    {
-        subject: "Matematika",
-        question: "Mennyi a tan 45° értéke?",
-        answer: ["1","0","√3","-1"],
-        correct: 0
     },
     {
         subject: "Magyar nyelv",
@@ -48,17 +42,24 @@ const kvizKerdes = [
         correct: 2
     },
     {
+        subject: "Történelem",
+        question: "Mi volt az áprilisi törvények egyik fő célja?",
+        answer: ["A Habsburg-ház megerősítése","A polgári átalakulás megteremtése","A három részre szakadt ország újraegyesítése","Az ország teljes függetlenségének kikiáltása"],
+        correct: 1
+    },
+    {
         subject: "Magyar nyelv",
         question: "Melyik szereplő mondja a híres gondolatot: „Mondottam, ember: küzdj és bízva bízzál!”?",
         answer: ["Ádám","Éva","Lucifer","Az Úr"],
         correct: 3
     },
     {
-        subject: "Magyar nyelv",
-        question: "Melyik történelmi korszak NEM jelenik meg a mű színeiben?",
-        answer: ["Ókori Athén","Középkori Prága","Római Birodalom","Amerikai vadnyugat"],
-        correct: 3
+        subject: "Matematika",
+        question: "Mennyi a tan 45° értéke?",
+        answer: ["1","0","√3","-1"],
+        correct: 0
     },
+
 
 
 ]
@@ -74,7 +75,8 @@ function kotkezo() {
         document.getElementById("kerdes3").innerText = kvizKerdes[szam].answer[2];
         document.getElementById("kerdes4").innerText = kvizKerdes[szam].answer[3];
         
-        
+
+
 
         szam++;
         document.getElementById("hanyadik").innerText = szam + "/10"
@@ -107,16 +109,34 @@ function kotkezo() {
         } else{
             td1.innerText = kvizKerdes[szam-2].question
             td2.innerText = "Helytelen válasz"
-            td3.innerText = kvizKerdes[szam-2].answer[bepipalt-1    ]
+            td3.innerText = kvizKerdes[szam-2].answer[bepipalt-1]
+            if(kvizKerdes[szam-2].answer[bepipalt-1] == undefined){
+                td3.innerText = "Nem adott meg választ"
+            }
         }
+
+        if(kvizKerdes[szam-2].subject == "Matematika"){
+            tr.className = "matek"
+        } else if(kvizKerdes[szam-2].subject == "Magyar nyelv"){
+            tr.className = "magyar"
+        } else {
+            tr.className = "tori"
+        }
+
         tr.append(td1)
         tr.append(td3)
         tr.append(td2)
         tbody.append(tr)
-        // Tablazat
+       
 
+        if (szam == 11){
+            alert("sizm")
+        }
 
-
+        document.getElementById("valasz1").checked = false
+        document.getElementById("valasz2").checked = false
+        document.getElementById("valasz3").checked = false
+        document.getElementById("valasz4").checked = false
 
 }
 }
